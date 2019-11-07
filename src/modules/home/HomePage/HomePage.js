c7f2eda8e5464cb3828fc09532b011b0c5b3a765
German Huayhua Condori <yhermmany@gmail.com>
2019-11-07 19:46:03 -0400

    Setup maps

--- src/modules/home/HomePage/HomePage.js
+++ src/modules/home/HomePage/HomePage.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import {stylesDefault} from './styles'
-import {Map, Marker} from 'google-maps-react';
+import Map from 'pigeon-maps';
+import Marker from 'pigeon-marker';
 
 export default class HomePage extends React.Component {
+  state = {
+    center: {
+      lat: -17.783259,
+      lng: -63.182214,
+    },
+    position: {
+      lat: -17.782309,
+      lng: -63.185393,
+    },
+    zoom: 15,
+  };
+
   render() {
+    const center = [this.state.center.lat, this.state.center.lng];
+    const position = [this.state.position.lat, this.state.position.lng];
     return (
-      <Map
-        google={this.props.google}
-        zoom={16}
-        style={stylesDefault.container}
-        initialCenter={{lat: -17.783259, lng: -63.182214}}
-      >
-        <Marker position={{lat: 17.782309, lng: -63.185393}}/>
-      </Map>
+      <div style={stylesDefault.container}>
+        <Map
+          center={center}
+          zoom={this.state.zoom}
+          height={500}
+        >
+          <Marker anchor={position} payload={1} onClick={({event, anchor, payload}) => {
+          }}/>
+        </Map>
+      </div>
     );
   }
 }
--- src/modules/home/HomePage/index.js
+++ src/modules/home/HomePage/index.js
@@ -1,6 +1,3 @@
 import HomePage from './HomePage';
-import {GoogleApiWrapper} from 'google-maps-react';
 
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyAZshCdlnhxfGrZNSOxkdsOdzM9hDhq2LQ'
-})(HomePage);
+export default HomePage;
