9cab3b1088e4a2080a05d28285c8c8dcca464ca0
Mauricio Ballesteros <mballes95@gmail.com>
2019-11-08 05:52:10 -0400

    Add Url

--- src/modules/home/HomePage/HomePage.js
+++ src/modules/home/HomePage/HomePage.js
@@ -1,90 +1,79 @@
-import React from 'react';
-import axios from 'axios';
-import { stylesDefault } from './styles'
-import Map from 'pigeon-maps';
-import Marker from 'pigeon-marker';
+import React from "react";
+import axios from "axios";
+import { stylesDefault } from "./styles";
+import Map from "pigeon-maps";
+import Marker from "pigeon-marker";
 
-import Popup from '../RegisterOlla/RegisterOlla';
-import MarkerInfo from '../MarkerInfo/MarkerInfo';
+import Popup from "../RegisterOlla/RegisterOlla";
+import MarkerInfo from "../MarkerInfo/MarkerInfo";
 const registerMessage = "¿Quieres registrar una olla común?";
-
-
 
 export default class HomePage extends React.Component {
   state = {
     center: {
       lat: -17.783259,
-      lng: -63.182214,
+      lng: -63.182214
     },
     zoom: 14,
     height: 0,
     places: [],
-    markerPressed: null,
+    markerPressed: null
   };
 
   componentDidMount() {
-    axios.get('https://192.168.0.2:8000/')
+    axios
+      .get(`http://${process.env.API_URL}/`)
       .then(res => {
         const places = res.data;
         this.setState({ places });
-      }).catch(error => {
-        console.log(error);
       })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   onMarkerPressed = ({ event, anchor, payload }) => {
     this.setState({
       markerPressed: payload
-    })
-  }
-  onCloseMarkerPressed= () =>{
+    });
+  };
+  onCloseMarkerPressed = () => {
     this.setState({
       markerPressed: null
-    })
-  }
+    });
+  };
   render() {
     const center = [this.state.center.lat, this.state.center.lng];
     return (
       <div style={stylesDefault.container}>
-        <Map
-          center={center}
-          zoom={this.state.zoom}
-          height={800}
-        >
-          {
-            this.state.places.map(
-              place => {
-
-                return (<Marker 
+        <Map center={center} zoom={this.state.zoom} height={800}>
+          {this.state.places.map(place => {
+            return (
+              <Marker
                 key={place.id}
-                anchor={[parseFloat(place.latitude), parseFloat(place.longitude)]} 
-
-                payload={place} 
-
-                onClick={this.onMarkerPressed} />)
-              }
-            )
-          }
+                anchor={[
+                  parseFloat(place.latitude),
+                  parseFloat(place.longitude)
+                ]}
+                payload={place}
+                onClick={this.onMarkerPressed}
+              />
+            );
+          })}
         </Map>
 
-        {this.props.showRegisterOlla ?
-          <Popup
-            text={registerMessage}
-            closePopup={this.props.togglePopup}
-          />
-          : null
-        }
+        {this.props.showRegisterOlla ? (
+          <Popup text={registerMessage} closePopup={this.props.togglePopup} />
+        ) : null}
 
-        {this.state.markerPressed ?
+        {this.state.markerPressed ? (
           <MarkerInfo
             title={this.state.markerPressed.name}
             description={this.state.markerPressed.description}
             closePopup={this.onCloseMarkerPressed}
           />
-          : null
-        }
+        ) : null}
       </div>
-
     );
   }
 }
