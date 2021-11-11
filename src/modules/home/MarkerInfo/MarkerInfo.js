b8f3f6aa2b0607cafd962a689adcb662a7aee245
Luis Fernando Morón Dorado <luifer.2341@gmail.com>
2021-11-10 21:18:38 -0400

    multiline support

--- src/modules/home/MarkerInfo/MarkerInfo.js
+++ src/modules/home/MarkerInfo/MarkerInfo.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import { stylesDefault } from './styles'
+import React from "react";
+import { stylesDefault } from "./styles";
 
 import Modal from "react-bootstrap/Modal";
 
 class MarkerInfo extends React.Component {
-    render() {
-        return (
-            <div style={stylesDefault.popup}>
-                <Modal.Dialog>
-                    <Modal.Header closeButton onClick={this.props.closePopup}>
-                        <Modal.Title>{this.props.title}</Modal.Title>
-                    </Modal.Header>
-
-                    <Modal.Body>
-                        <p style={{whiteSpace: "pre-wrap" }}>{this.props.description}</p>
-                    </Modal.Body>
-
-                </Modal.Dialog>
-            </div>
-        );
+  render() {
+    function NewlineText(props) {
+      const text = props.text.toString()+ "";
+      const newText = text.split("\\n").map((str,index) => <p key={index.toString()}>{str}</p>);
+      return newText;
     }
+
+    return (
+      <div style={stylesDefault.popup}>
+        <Modal.Dialog>
+          <Modal.Header closeButton onClick={this.props.closePopup}>
+            <Modal.Title>{this.props.title}</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            {<NewlineText text={this.props.description} />}
+          </Modal.Body>
+        </Modal.Dialog>
+      </div>
+    );
+  }
 }
 
 export default MarkerInfo;
