f9f7c038c1d412bca676c4e22979eeeb35ba88b8
jgtorrejon <joel.torrejon.mendez@gmail.com>
2021-11-13 12:12:01 -0400

    feat(marker): add button to redirect to maps

--- src/modules/home/MarkerInfo/MarkerInfo.js
+++ src/modules/home/MarkerInfo/MarkerInfo.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from 'react-bootstrap/Button'
+
 import { stylesDefault } from "./styles";
 
-import Modal from "react-bootstrap/Modal";
-
 class MarkerInfo extends React.Component {
+  getMapsLink(lat, long) {
+    return `https://www.google.com/maps/search/?api=1&query=${lat},${long}`;
+  }
+
+  getDrivePhotoLink(photoId) {
+    return `https://drive.google.com/thumbnail?id=${photoId}`;
+  } 
+
   render() {
     function NewlineText(props) {
       const text = props.text.toString()+ "";
@@ -21,6 +30,9 @@ class MarkerInfo extends React.Component {
           <Modal.Body>
             {<NewlineText text={this.props.description} />}
           </Modal.Body>
+          <Modal.Footer>
+            <Button href={this.getMapsLink(this.props.latitude, this.props.longitude)} variant="success">Ir a google maps</Button>
+          </Modal.Footer>
         </Modal.Dialog>
       </div>
     );
