b06d6523a8f213a9f7dbec027c271e98633af32e
luifermoron <contact@luifermoron.com>
2022-02-08 13:09:54 -0400

    react boostrap from home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,44 +1,31 @@
 import styles from "./Home.module.scss";
-import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-
 import Map from "../../components/Map/Map";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
 
 const Home = () => {
   return (
     <div>
-      <Navbar bg="light" variant="light" style={{ color: "green" }}>
-        <Container>
-          <Navbar.Brand>
-            <Link style={{ color: "green" }} to={"/"}>
-              {" "}
-              ¡Apoyemos a nuestra gente!{" "}
-            </Link>
-          </Navbar.Brand>
-          <Nav className="mr-auto"></Nav>
-          <Button
-            variant="outline-primary"
-            style={{ color: "green", borderColor: "green" }}
-            className="btn-hover"
-          >
+      <nav>
+        <div className={styles.welcome}>
+          <Link className={styles.btn} to={"/"}>
             {" "}
-            <a
-              style={{ color: "green" }}
-              rel="noopener"
-              target="_blank"
-              href="https://forms.gle/Qj1m4XVAL37winH99"
-            >
-              Registrar olla común
-            </a>
-          </Button>
-        </Container>
-      </Navbar>
-      <Map/>
+            ¡Apoyemos a nuestra gente!{" "}
+          </Link>
+        </div>
+
+        <div className={styles.signup}>
+          <a
+            className={styles.btn}
+            rel="noopener"
+            target="_blank"
+            href="https://forms.gle/Qj1m4XVAL37winH99"
+          >
+            Registrar olla común
+          </a>
+        </div>
+      </nav>
+      <Map />
     </div>
   );
 };
